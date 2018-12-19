1a706e6411d484f072073f684b352f6a49dbc2e9
AJ Caldwell <tomatrow@gmail.com>
2018-12-19 13:14:08 -0800

    Use OrbitApp in cube example

--- examples/scripts/cube/main.js
+++ examples/scripts/cube/main.js
@@ -1,19 +1,6 @@
 // This code is from discoverthree.com-examples by `looeee` on Github.
-const app = new App('container')
+const app = new OrbitApp('container')
 window.app = app
-
-function initLights() {
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-    app.scene.add(ambientLight);
-
-    const frontLight = new THREE.DirectionalLight(0xffffff, 1);
-    frontLight.position.set(10, 10, 10);
-
-    const backLight = new THREE.DirectionalLight(0xffffff, 1);
-    backLight.position.set(-10, 10, -10);
-
-    app.scene.add(frontLight, backLight);
-}
 
 function initMeshes() {
     // create a geometry
@@ -43,12 +30,6 @@ function init() {
 
     app.init();
 
-    app.scene.background = new THREE.Color(0x8FBCD4);
-    app.camera.position.set(0, 0, 10);
-    app.camera.far = 100;
-    app.camera.updateProjectionMatrix();
-
-    initLights();
     initMeshes();
 
     app.start();
