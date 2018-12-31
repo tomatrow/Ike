2002211850727cc5ff04d6e5a259e6e41540afb3
AJ Caldwell <tomatrow@gmail.com>
2018-12-31 13:55:17 -0800

    Plan out the arm
    The goal is really the same as the examples from Three.IK, but with my own library.
    
    Signed-off-by: AJ Caldwell <tomatrow@gmail.com>

--- examples/scripts/arm/main.js
+++ examples/scripts/arm/main.js
@@ -2,11 +2,36 @@
 const app = new OrbitApp('container')
 window.app = app
 
-function initMeshes() {}
+function createCube(radius = 1) {
+    return new THREE.Mesh()
+}
+
+/* Crates an Arm with `segmentCount+1` joints.
+ * The start is the _base_ and the end is the _tip_.
+ */
+function createArm(segmentLength = 2, segmentCount = 2) {
+    return new THREE.Object3D()
+}
+
+/* Creates an easily recognizable skinned arm.
+ * Each joint has a bone. The object graph is: root -> [skeleton,cubes]
+ * where |skeleton| == |cubes|.
+ * createJoint is (radius: Int) -> Mesh
+ */
+function initArm(createJoint = createCube) {
+    return new THREE.Object3D()
+}
+
+function initGraph() {
+    const arm = initArm()
+    app.scene.add(arm)
+}
 
 function init() {
 
     app.init();
+
+    initGraph()
 
     app.container.addEventListener('click', () => {
         app.running ? app.stop() : app.start();
