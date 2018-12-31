60dd2dff35503018ca3fd70958a86c84fee66d03
AJ Caldwell <tomatrow@gmail.com>
2018-12-31 13:26:29 -0800

    Update Arm for OrbitApp setup
    I’m about to use this to create an arm visualized with three cubes.
    
    Signed-off-by: AJ Caldwell <tomatrow@gmail.com>

--- examples/scripts/arm/main.js
+++ examples/scripts/arm/main.js
@@ -1,19 +1,6 @@
 // This code is from discoverthree.com-examples by `looeee` on Github.
-const app = new App('container')
+const app = new OrbitApp('container')
 window.app = app
-
-function initLights() {
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-    app.scene.add(ambientLight);
-
-    const frontLight = new THREE.DirectionalLight(0xffffff, 1);
-    frontLight.position.set(10, 10, 10);
-
-    const backLight = new THREE.DirectionalLight(0xffffff, 1);
-    backLight.position.set(-10, 10, -10);
-
-    app.scene.add(frontLight, backLight);
-}
 
 function initMeshes() {}
 
@@ -21,19 +8,11 @@ function init() {
 
     app.init();
 
-    app.scene.background = new THREE.Color(0x8FBCD4);
-    app.camera.position.set(0, 0, 10);
-    app.camera.far = 100;
-    app.camera.updateProjectionMatrix();
-
-    initLights();
-    initMeshes();
-
-    app.start();
-
     app.container.addEventListener('click', () => {
         app.running ? app.stop() : app.start();
     });
+
+    app.start();
 }
 
 // call the init function to set everything up
