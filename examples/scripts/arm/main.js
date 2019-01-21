a3879de0408cbc4630c734d6d39f2e271d33de6e
AJ Caldwell <tomatrow@gmail.com>
2019-01-21 13:44:13 -0800

    Remove unneded code
    Signed-off-by: AJ Caldwell <tomatrow@gmail.com>

--- examples/scripts/arm/main.js
+++ examples/scripts/arm/main.js
@@ -2,47 +2,7 @@
 const app = new OrbitApp('container')
 window.app = app
 
-class Arm extends THREE.SkinnedMesh {
-    constructor(segmentLength = 2, segmentCount = 2, createJointGeometry = () => new THREE.BoxBufferGeometry(1, 1, 1)) {
-
-        const geometry = Arm.createGeometry(createJointGeometry, segmentLength, segmentCount)
-        const material = new THREE.MeshBasicMaterial({
-            color: 0xffff00
-        });
-        super(geometry, material)
-
-        // make the armature
-        // TODO: use geometry.groups to define the control each bone should have
-        const root = new THREE.Bone()
-        const skeleton = new THREE.Skeleton([root])
-
-        this.add(root)
-        this.bind(skeleton)
-    }
-
-    // creates a chain of size segmentCount+1
-    static createGeometry(createJointGeometry, segmentLength, segmentCount) {
-        const geometries = []
-        for (let i = 0; i <= segmentCount; i++) {
-            const jointGeometry = createJointGeometry()
-            const matrix = new THREE.Matrix4().makeTranslation(0, i * segmentLength, 0)
-            jointGeometry.applyMatrix(matrix)
-            geometries.push(jointGeometry)
-        }
-        const geometry = THREE.BufferGeometryUtils.mergeBufferGeometries(geometries, true)
-        return geometry
-    }
-
-}
-
-
 function initArm() {
-    const arm = new Arm()
-    return arm
-}
-
-function initGraph() {
-
     // create segments
     const root = new THREE.Object3D()
     const elbow = new THREE.Object3D()
@@ -57,8 +17,13 @@ function initGraph() {
     elbow.position.setY(2)
     hand.position.setY(2)
 
-    // test this chain
-    app.scene.add(createAutoSkinnedMesh(root))
+    const arm = createAutoSkinnedMesh(root)
+    return arm
+}
+
+function initGraph() {
+    const arm = initArm()
+    app.scene.add(arm)
 }
 
 function init() {
