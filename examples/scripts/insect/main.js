e2420316dd391c640d29d1f7e69068c87a35c1e5
AJ Caldwell <tomatrow@gmail.com>
2019-01-30 15:54:02 -0800

    Import a stick bug without vertex groups
    Signed-off-by: AJ Caldwell <tomatrow@gmail.com>

--- examples/scripts/insect/main.js
+++ examples/scripts/insect/main.js
@@ -1,7 +1,21 @@
 const app = new OrbitApp('container')
 window.app = app
 
-function initGraph() {}
+async function initInsect() {
+    const path = 'assets/Stick Closed.gltf'
+    const importedObject = await load(path)
+    // we are trying to import an armature initially,
+    // the actual mesh is one of it's children
+    const insectMesh = importedObject.getObjectByName('Body')
+    return insectMesh
+}
+
+async function initGraph() {
+    // import the stick bug
+    const insect = await initInsect()
+    // add to scene
+    app.scene.add(insect)
+}
 
 function init() {
 
